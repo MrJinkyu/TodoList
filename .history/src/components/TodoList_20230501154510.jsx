1c0b484fd59bc8b8e120a8c2330d8cb2a8f60204
MrJinkyu <rlawlsrb167@naver.com>
2023-05-01 15:49:45 +0900

    로컬 스토리지에 저장하기 기능 구현

--- .history/src/components/TodoList_20230501154510.jsx
+++ .history/src/components/TodoList_20230501154510.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 import AddTodo from './AddTodo';
 import Todo from './Todo';
 import styles from './TodoList.module.css';
